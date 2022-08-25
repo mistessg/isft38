151f7de368c18b08135120a89ff1590bf8e727ce
renovacionweb38 <112045817+renovacionweb38@users.noreply.github.com>
2022-08-25 00:04:47 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ use App\Http\Controllers\SedetelefonosController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
