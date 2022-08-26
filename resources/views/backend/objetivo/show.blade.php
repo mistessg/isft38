967803792d60a58e87f27140258e47a82882f4a9
estehub <90066710+estehub@users.noreply.github.com>
2022-08-25 21:39:07 -0300

    Show esteban

--- resources/views/backend/objetivo/show.blade.php
+++ resources/views/backend/objetivo/show.blade.php
@@ -11,18 +11,18 @@
    {{ Form::model($carrera, [ 'method' => 'get' , 'route' => ['carreras.show', $carrera->id]]) }}
     <div class="form-group @if($errors->has('carrera')) has-error has-feedback @endif">
           {{ Form::label("carrera", 'Carrera', ['class' => 'control-label']) }}
-          {{Form::text("carrera", null , ["class" => "form-control", "readonly" ])}}                        
+          {{Form::text("carrera", null , ["class" => "form-control", "readonly" ])}}
     </div>
     <div class="form-group">
           {{ Form::label("resolucion", 'Resolución', ['class' => 'control-label']) }}
-          {{Form::text("resolucion", old("cresolucion"), ["class" => "form-control", "readonly" ])}}                        
+          {{Form::text("resolucion", old("cresolucion"), ["class" => "form-control", "readonly" ])}}
           @error('resolucion')
               <div class="alert alert-danger">{{ $message }}</div>
           @enderror
-    </div>    
+    </div>
     <div class="form-group">
           {{ Form::label("res_archivo", 'Resolución PDF', ['class' => 'control-label']) }}
           @if($carrera->res_archivo)<span class="badge badge-light"><a href="{{ asset('./storage/'. $carrera->res_archivo) }}">{{ basename($carrera->res_archivo) }}</a> </span> @endif
-    </div>   
+    </div>
 {!!Form::close()!!}
 @endsection
