44cd0d6a3382916e774dac7639c94f57b1f8dfc0
Martín Teves <martinteves14@gmail.com>
2022-09-01 18:07:09 -0300

    commit carreras materias

--- resources/views/backend/materia/create.blade.php
+++ resources/views/backend/materia/create.blade.php
@@ -7,37 +7,7 @@
     <div class="alert alert-success">{{ Session('status')}}</div>
     @endif
   </div>
-  <div class="links">
- {{ Form::open(['route' => 'materias.store', 'files' => true]) }}
-  @csrf <!-- {{ csrf_field() }} -->
-    <div class="form-group">
-          {{ Form::label("materia", 'Materia', ['class' => 'control-label']) }}
-          {{Form::text("materia", old("materia"), ["class" => "form-control", "placeholder" => "Ingrese la Materia", ])}}                        
-          @error('materia')
-              <div class="alert alert-danger">{{ $message }}</div>
-          @enderror
-    </div>
-    <div class="form-group">
-          {{ Form::label("carrera_id", __('noticias.carrera'), ['class' => 'control-label']) }}
-          {{Form::select("carrera_id", $carreras, null, ["class" => "form-control", "placeholder" => "Seleccione una carrera"]) }}           
-          @error('carrera_id')
-              <div class="alert alert-danger">{{ $message }}</div>
-          @enderror 
-    </div>     
-    <div class="form-group">
-          {{ Form::label("anio", 'Año', ['class' => 'control-label']) }}
-          {{Form::select("anio", $anio, '1', ["class" => "form-control", "placeholder" => "Seleccione un año"]) }}                       
-          @error('anio')
-              <div class="alert alert-danger">{{ $message }}</div>
-          @enderror
-    </div>     
-    <div class="form-group">
-          {{ Form::label("contenidos", 'Programa', ['class' => 'control-label']) }}
-           {{ Form::file("contenidos") }}                  
-          @error('contenidos')
-              <div class="alert alert-danger">{{ $message }}</div>
-          @enderror
-    </div>       
-    </br><button type="submit" style="width: 100%;" class="btn btn-primary">Guardar</button></div>
-{!!Form::close()!!}
+  @foreach($materias as $materia)
+    {{ $materia }}
+@endforeach
 @endsection
