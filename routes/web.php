4fe53503570cda4683a32be97f68086d8983e0e1
estehub <90066710+estehub@users.noreply.github.com>
2022-09-01 20:26:32 -0300

    Merge web.php

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,44 @@ Route::resource('objetivo', objetivoController::class);
 Route::resource('profesor', ProfesorController::class);
 Route::resource('programa', ProgramaController::class);
 Route::resource('sede', SedeController::class);
+
+
+----
+@@ -37,6 +37,16 @@ Route::get('/', function () {
+    | Resource
+    |--------------------------------------------------------------------------
+    */
+    <<<<<<< HEAD
+    =======
+    //programa
+    Route::get('/programa/cargarPrograma', [ProgramaController::class, 'CargarPrograma']);
+    Route::get('/programa/programasPendientes', [ProgramaController::class, 'ProgramasPendientes']);
+    Route::post('/programa/search', [ProgramaController::class, 'search'])->name('programa.search');
+    Route::resource('programa', ProgramaController::class);
+    Route::post('horario/search', [HorarioController::class, 'search'])->name('horario.search');;
+
+    >>>>>>> cc1f91406776d62ba0d7152a9117b1a0e9fa0367
+    Route::resource('anio', AnioController::class);
+    Route::resource('carrera', CarreraController::class);
+    Route::resource('comision', ComisionController::class);
+    @ -48,3 +58,20 @@ Route::resource('objetivo', objetivoController::class);
+    Route::resource('profesor', ProfesorController::class);
+    Route::resource('programa', ProgramaController::class);
+    Route::resource('sede', SedeController::class);
+    <<<<<<< HEAD
+    =======
+
+    //horarios
+    Route::get('/horarios/porProfesor', [HorarioController::class, 'porProfesor']);
+    Route::get('/horarios/porCarrera', [HorarioController::class, 'porCarrera']);
+    Route::get('/horarios/porDiaHora', [HorarioController::class, 'porDiaHora']);
+
+
+    Auth::routes();
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Auth::routes();
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    >>>>>>> cc1f91406776d62ba0d7152a9117b1a0e9fa0367
