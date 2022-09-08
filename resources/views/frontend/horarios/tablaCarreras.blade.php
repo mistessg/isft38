fc60cf46153d66a96225749d269b474bce86f206
Ulises Dysiewicz <52222682+ulisesdysiewicz@users.noreply.github.com>
2022-09-07 21:46:03 -0300

    Show horarios - View Profesor

--- resources/views/frontend/horarios/tablaCarreras.blade.php
+++ resources/views/frontend/horarios/tablaCarreras.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Horarios por Carreras')
 
-@section('content')
+<!-- @section('content')
 <table class="table table-striped container mx-auto p-2" >
         <tr class="bg-secondary text-light bg-gradient">
             <th class="text-center" scope="col">HORARIO</th>
@@ -22,7 +22,90 @@
             <td  class="text-center">HORARIO</td>
             <td  class="text-center">HORARIO</td>
         </tr>
+</table> -->
+
+{{ Form::open(['route' => 'horario.store']) }}
+<div class="input-group mb-3">
+    <label class="input-group-text" for="#">Sede</label>
+    {{Form::text("sede", $sede->descripcion , ["class" => "form-control", "readonly" ])}}
+    {{Form::text("sede_id", $sede->id , ["class" => "form-control", "hidden" ])}}
+
+</div>
+
+<div class="input-group mb-3">
+    <label class="input-group-text" for="#">Carrera</label>
+    {{Form::text("carrera", $carrera->descripcion , ["class" => "form-control", "readonly" ])}}
+    {{Form::text("carrera_id", $carrera->id , ["class" => "form-control", "hidden" ])}}
+
+</div>
+
+
+</br><button type="submit" style="width: 100%;" class="btn btn-primary">Consultar</button></div>
+{!!Form::close()!!}
+
+@foreach($horarios as $horario)
+@if($loop->first)
+<table class="table table-dark">
+    <tr>
+        <th class="text-center" scope="col">HORARIO</th>
+        @foreach($dias as $dia)
+        <th class="text-center" scope="col">{{$dia}}</th>
+        @endforeach
+     </tr>
+     @endif
+        @foreach($horarios as $horario)
+        
+        <tr>
+
+        <td>{{$horario->moduloHorario->horainicio}} - {{$horario->moduloHorario->horafin}}</td>
+        <td>{{$horario->materia->descripcion}} {{$horario->profesor->apellido}}, {{$horario->profesor->nombre}}<br> 
+        <a href="{{ route('horario.create') }}" class="btn btn-success">
+        <img src="{{ asset('svg/new.svg') }}" height="20" width="20" alt="Crear" title="Crear">
+        </a></td>
+        <td>{{$horario->materia->descripcion}} {{$horario->profesor->apellido}}, {{$horario->profesor->nombre}}<br> 
+        <a href="{{ route('horario.create') }}" class="btn btn-success">
+        <img src="{{ asset('svg/new.svg') }}" height="20" width="20" alt="Crear" title="Crear">
+        </a></td>
+        <td>{{$horario->materia->descripcion}} {{$horario->profesor->apellido}}, {{$horario->profesor->nombre}}<br> 
+        <a href="{{ route('horario.create') }}" class="btn btn-success">
+        <img src="{{ asset('svg/new.svg') }}" height="20" width="20" alt="Crear" title="Crear">
+        </a></td>
+        <td>{{$horario->materia->descripcion}} {{$horario->profesor->apellido}}, {{$horario->profesor->nombre}}<br> 
+        <a href="{{ route('horario.create') }}" class="btn btn-success">
+        <img src="{{ asset('svg/new.svg') }}" height="20" width="20" alt="Crear" title="Crear">
+        </a></td>
+        <td>{{$horario->materia->descripcion}} {{$horario->profesor->apellido}}, {{$horario->profesor->nombre}}<br> 
+        <a href="{{ route('horario.create') }}" class="btn btn-success">
+        <img src="{{ asset('svg/new.svg') }}" height="20" width="20" alt="Crear" title="Crear">
+        </a></td>
+        <td>{{$horario->materia->descripcion}} {{$horario->profesor->apellido}}, {{$horario->profesor->nombre}}<br> 
+        <a href="{{ route('horario.create') }}" class="btn btn-success">
+        <img src="{{ asset('svg/new.svg') }}" height="20" width="20" alt="Crear" title="Crear">
+        </a></td>
+
+        <!-- <td>{{$horario->dia }}</td>
+        <td>{{$horario->moduloHorario->horainicio}} - {{$horario->moduloHorario->horafin}}</td>
+        <td>{{$horario->materia->descripcion}}</td>
+        <td>{{$horario->profesor->apellido}}, {{$horario->profesor->nombre}}</td>
+        <td>{{$horario->comentario}}</td>
+        <td>{{$horario->comision->comision}}</td>
+        <td>
+            {{ Form::model($horario, [ 'method' => 'delete', 'route' => ['horario.destroy', $horario -> id] ]) }}
+            @csrf
+            <a href="{{ route('horario.edit', ['horario' =>  1] ) }}" class="btn btn-primary">
+                <img src="{{ asset('svg/edit.svg') }}" width="20" height="20" alt="Editar" title="Editar">
+            </a>
+            <button type="submit" class="btn btn-danger" onclick="if (!confirm('¿Esta seguro de borrar el horario?')) return false;">
+                <img src="{{ asset('svg/delete.svg') }}" width="20" height="20" alt="Borrar" title="Borrar">
+            </button>
+            {!!Form::close()!!}
+        </td> -->
+    </tr>
+    @if($loop->last)
 </table>
+@endif
+
+@endforeach
 
 
 @endsection
---
+++ resources/views/frontend/profesor/login.php
@@ -0,0 +1,35 @@
+@extends('backend.layouts.main')
+
+@section('title', 'Login Profesores')
+
+@section('content')
+
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-md">
+        <a class="navbar-brand" href="#">Cargar horarios</a>
+      </div>
+    </nav>
+
+    <div class="container my-4">
+    
+        <div class="card">
+        <h5 class="card-header" style=" background-color: #181818; color: white;">Login</h5>
+        <div class="card-body">
+
+        <div class="mb-3">
+        <label for="exampleInputUser" class="form-label">Usuario</label>
+        <input type="text" class="form-control" id="exampleInputUser" aria-describedby="userHelp">
+        </div>
+
+        <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+    
+        <div class="d-grid gap-2">
+        <button class="btn btn-outline-dark" type="submit" aria-label="consultar">Consultar</button>
+        </div>
+          
+    </div>
+
+@endsection
