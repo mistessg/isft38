c12f16e80a09b9692a791dbb9ec0134a01e9943d
renovacionweb38 <112045817+renovacionweb38@users.noreply.github.com>
2022-09-08 00:20:31 -0300

    Novedades - Gisela

--- resources/views/frontend/carrousel/carrousel.blade.php
+++ resources/views/frontend/carrousel/carrousel.blade.php
@@ -36,7 +36,28 @@
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
-    <div class="carousel-item active">
+  @foreach($novedades as $novedad)
+  @if($loop->first)  
+  <div class="carousel-item active">
+  @else
+  <div class="carousel-item">
+  @endif
+      <img src="https://img.freepik.com/vector-premium/fondo-cuadrado-negro-geometrico-relieve_51543-519.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <h5>{{$novedad->titulo}}</h5>
+        @if($novedad->imagen)
+            @if(Str::startsWith($novedad->imagen, 'http'))
+              <img src="{{ $novedad->imagen }}" class="img-fluid img-thumbnail" alt="...">
+            @else
+              <img src="{{ asset('./storage/'. $novedad->imagen) }}" class="img-fluid img-thumbnail" alt="...">  
+            @endif
+         @endif      
+       
+        <p>{!!substr($novedad->cuerpo, 0, 300)!!}... <a href="{{route('blog.noticias.leer',$novedad->id)}}" target="_blank">Seguir Leyendo »</a></p>
+      </div>
+    </div>
+    @endforeach
+    <div class="carousel-item">
       <img src="https://www.diferencias.cc/wp-content/uploads/2021/06/diferencia-entre-facultad-y-universidad.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
