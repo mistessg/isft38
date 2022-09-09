153a72ae739d1af786ed2ac3e9875871117f7ca7
aladner <ladnerale@gmail.com>
2022-09-08 21:06:28 -0300

    Programa Controller Store

--- app/Http/Controllers/ProgramaController.php
+++ app/Http/Controllers/ProgramaController.php
@@ -54,11 +54,7 @@ class ProgramaController extends Controller
         return view('frontend.programa.programas_pendientes', compact('programas'));
     }
 
-    public function store(Request $request){
-        $programa = new Programa(){
-            
-        }
-    }
+   
 
     /**
      * Show the form for creating a new resource.
@@ -78,7 +74,24 @@ class ProgramaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $programa = new Programa();
+        $programa->sede_id = $request->input('sede_id');
+        $programa->carrera_id = $request->input('carrera_id');
+        $programa->anio_id = $request->input('anio_id');
+        $programa->materia_id = $request->input('materia_id');
+        $programa->comision_id = $request->input('comision_id');
+        $programa->profesor_id = $request->input('profesor_id');
+        $programa->fechaentrega = $request->input('fechaentrega');
+
+        $programa->save();
+
+        if ($request->hasFile('nombrearchivo')) {
+            $archivo = $request->file('nombrearchivo');
+            $path = $archivo->storeAs('public/programa/' . $programa->id, $archivo->getClientOriginalName() ); 
+            $savedPath  =str_replace("public/", "", $path);
+            $programa->nombrearchivo = $savedPath;   
+            $programa->save();   
+       } 
     }
 
         /**
