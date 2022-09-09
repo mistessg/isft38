8cc097c4b032ff43390fffffc2b961bde5285945
estehub <90066710+estehub@users.noreply.github.com>
2022-09-08 21:02:17 -0300

    ABM Historia

--- resources/views/backend/historia/show.blade.php
+++ resources/views/backend/historia/show.blade.php
@@ -1,28 +1,16 @@
 @extends('backend.layouts.main')
 @section('title', 'Historia')
 @section('content')
-  <h1>Carrera</h1>
-  <div>
-    @if(Session::has('status'))
-    <div class="alert alert-success">{{ Session('status')}}</div>
-    @endif
-  </div>
+  <h1>Historia</h1>
   <div class="links">
-   {{ Form::model($carrera, [ 'method' => 'get' , 'route' => ['carreras.show', $carrera->id]]) }}
-    <div class="form-group @if($errors->has('carrera')) has-error has-feedback @endif">
-          {{ Form::label("carrera", 'Carrera', ['class' => 'control-label']) }}
-          {{Form::text("carrera", null , ["class" => "form-control", "readonly" ])}}
-    </div>
-    <div class="form-group">
-          {{ Form::label("resolucion", 'Resolución', ['class' => 'control-label']) }}
-          {{Form::text("resolucion", old("cresolucion"), ["class" => "form-control", "readonly" ])}}
-          @error('resolucion')
+   {{ Form::model($historia, [ 'method' => 'get' , 'route' => ['historia.show', $historia->id]]) }}
+   <div class="form-group">
+          {{ Form::label("historia", 'Historia', ['class' => 'control-label']) }}
+          {{ Form::textarea("historia", old("historia"), ["class" => "form-control", "placeholder" => "Ingrese la Historia" , "readonly" ]) }}
+          @error('historia')
               <div class="alert alert-danger">{{ $message }}</div>
           @enderror
     </div>
-    <div class="form-group">
-          {{ Form::label("res_archivo", 'Resolución PDF', ['class' => 'control-label']) }}
-          @if($carrera->res_archivo)<span class="badge badge-light"><a href="{{ asset('./storage/'. $carrera->res_archivo) }}">{{ basename($carrera->res_archivo) }}</a> </span> @endif
-    </div>
+
 {!!Form::close()!!}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,7 @@ Route::group([], __DIR__.'/contacto.php');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
