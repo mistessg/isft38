da34722515b44a71829c31ba362ae2f478d8e6c0
estehub <90066710+estehub@users.noreply.github.com>
2022-09-14 18:59:33 -0300

    Update Objetivo

--- app/Models/Objetivo.php
+++ app/Models/Objetivo.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Objetivo extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'objetivo',
+    ];
+
+public function sede(){
+    return $this->belongsTo(Sede::class, 'sede_id');
+}
 }
