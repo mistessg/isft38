ae337b38da79a87a091214308404550cf59a76d7
Ulises Dysiewicz <52222682+ulisesdysiewicz@users.noreply.github.com>
2022-09-14 19:10:18 -0300

    horarioeditblade backend

--- resources/views/backend/horario/edit.blade.php
+++ resources/views/backend/horario/edit.blade.php
@@ -8,28 +8,77 @@
     @endif
   </div>
   <div class="links">
-{{ Form::model($carrera, [ 'method' => 'put' , 'route' => ['carreras.update', $carrera->id],  'files' => true]) }}
+
+{{ Form::model($horarios, [ 'method' => 'put' , 'route' => ['horario.update', $horarios->id],  'files' => true]) }}
+
   @csrf <!-- {{ csrf_field() }} -->
-    <div class="form-group @if($errors->has('titulo')) has-error has-feedback @endif">
-          {{ Form::label("carrera", 'Carrera', ['class' => 'control-label']) }}
-          {{Form::text("carrera", old("carrera"), ["class" => "form-control", "placeholder" => "Ingrese la Carrera", ])}}                        
-          @error('carrera') <div class="alert alert-danger">{{ $message }}</div>@enderror
-    </div>
-    <div class="form-group">
-          {{ Form::label("resolucion", 'Resolución', ['class' => 'control-label']) }}
-          {{Form::text("resolucion", old("cresolucion"), ["class" => "form-control", "placeholder" => "Ingrese la Resolucion", ])}}                        
-          @error('resolucion')
-              <div class="alert alert-danger">{{ $message }}</div>
-          @enderror
-    </div>    
-    <div class="form-group">
-          {{ Form::label("res_archivo", 'Resolución PDF', ['class' => 'control-label']) }}
-           {{ Form::file("res_archivo") }}                  
-          @error('res_archivo')
-              <div class="alert alert-danger">{{ $message }}</div>
-          @enderror
-    </div>     
- </br><button type="submit" style="width: 100%;" class="btn btn-primary">Guardar</button>    
-    </div>
+
+  <div class="container my-4">
+    <div class="card">
+    <h5 class="card-header" style=" background-color: #181818; color: white;">Cargar horarios</h5>
+    <div class="card-body">
+    {{ Form::open(['route' => 'horario.store']) }}
+    <div class="input-group mt-5 mb-3">
+    <label class="input-group-text" for="#">Sede</label>
+    {{Form::text("sede", $sede->descripcion , ["class" => "form-control", "readonly" ])}}
+    {{Form::text("sede_id", $sede->id , ["class" => "form-control", "hidden" ])}}
+  </div>
+
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Carrera</label>
+  {{Form::text("carrera", $carrera->descripcion , ["class" => "form-control", "readonly" ])}}
+  {{Form::text("carrera_id", $carrera->id , ["class" => "form-control", "hidden" ])}}
+  </div>
+
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Año</label>
+  {{Form::text("anio", $anio->descripcion , ["class" => "form-control", "readonly" ])}}
+  {{Form::text("anio_id", $anio->id , ["class" => "form-control", "hidden" ])}}
+
+  </div>
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Comision</label>
+  {{Form::text("comision", $comision->comision , ["class" => "form-control", "readonly" ])}}
+  {{Form::text("comision_id", $comision->id , ["class" => "form-control", "hidden" ])}}
+  </div>
+
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Día</label>
+  @if(empty($dia))
+    {{Form::select("dia", $dias, null, ["class" => "form-control", "placeholder" => "Seleccione un día"]) }}   
+  @else
+    {{Form::text("dia_nombre", $dias[$dia] , ["class" => "form-control", "readonly" ])}}
+    {{Form::text("dia", $dia , ["class" => "form-control", "hidden" ])}}
+  @endif 
+  </div>   
+
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Horario</label>
+  @if(empty($modulo))
+  {{Form::select("modulohorario_id", $modulosHorario, null, ["class" => "form-control", "placeholder" => "Seleccione un   horario"]) }}   
+  @else
+  {{Form::text("modulohorario", $modulo->horainicio . ' - ' . $modulo->horafin , ["class" => "form-control", "readonly" ])}}
+  {{Form::text("modulohorario_id", $modulo->id , ["class" => "form-control", "hidden" ])}}
+  @endif         
+  </div>
+
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Materia</label>
+  {{Form::select("materia_id", $materias, null, ["class" => "form-control", "placeholder" => "Seleccione una materia"]) }}   
+  </div>
+
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Profesor</label>
+  {{Form::select("profesor_id", $profesores, null, ["class" => "form-control", "placeholder" => "Seleccione un  profesor"]) }}   
+  </div>   
+
+  <div class="input-group mb-3">
+  <label class="input-group-text" for="#">Comentario</label>
+  {{Form::text("comentario", null , ["class" => "form-control" ])}}
+  </div>   
+      </br><button type="submit" style="width: 100%;" class="btn btn-primary">Agregar</button></div>
+       {!!Form::close()!!}  
+  </div> 
+
 {!!Form::close()!!}
 @endsection
