4a8c10169d884b960af7c7eb34227c80612b1dd8
Ulises Dysiewicz <52222682+ulisesdysiewicz@users.noreply.github.com>
2022-09-15 19:44:38 -0300

    show-horario uli

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -21,18 +21,15 @@
 </head>
 
 <body>
-  <nav class="navv navbar navbar navbar-expand-lg " style="background-color: #F5F5F5; color: white;">
+  <nav class="navv navbar navbar navbar-expand-lg bg-dark " style="background-color: #F5F5F5; color: white;">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"><img src="	http://www.isft38.edu.ar/image/logo.png" width="35px" height="60px" alt="" srcset=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ms-auto">
-          <a class="nav-link active" href="/">Inicio</a>
-          <a class="nav-link" href="/carrera">Carreras</a>
-          <a class="nav-link" href="#">Horarios</a>
-          <a class="nav-link" href="/contacto">Contacto</a>
+        <div class="navbar-nav ms-auto text-light">
+          <a class="nav-link active text-light" href="/">Inicio</a>
         </div>
       </div>
     </div>
