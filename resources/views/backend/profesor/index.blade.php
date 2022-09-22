186bfe3b00ed7d8bb5d47798d6881dddba40bef1
Ulises Dysiewicz <52222682+ulisesdysiewicz@users.noreply.github.com>
2022-09-22 20:57:40 -0300

    vista index profesor

---
+++ resources/views/backend/profesor/index.blade.php
@@ -0,0 +1,42 @@
+@extends('backend.layouts.main')
+@section('title', 'Profesores')
+@section('content')
+
+@forelse($profesores as $profesor)
+    @if($loop->first)
+        <table class="table table-dark">
+            <tr>
+                 <td>Nombre</td>
+                 <td>Apellido</td>
+                 <td>
+                    <a href="{{ route('profesor.create') }}" class="btn btn-success">
+                        <img src="{{ asset('svg/new.svg') }}" height="20" width="20" alt="Crear" title="Crear">
+                    </a>
+                </td>
+            </tr>
+    @endif
+            <tr>
+                <td>{{ $profesor->nombre}}</td>
+                <td>{{ $profesor->apellido}}</td>
+                <td>
+                <!--<a href="{{ route('profesor.show', ['profesor' => $profesor->id ] ) }}" class="btn btn-info">
+                        <img src="{{ asset('svg/show.svg') }}" width="20" height="20" alt="Mostrar" title="Mostrar">
+                    </a>-->
+                    <a href="{{ route('profesor.edit', ['profesor' => $profesor->id ] ) }}" class="btn btn-primary">
+                        <img src="{{ asset('svg/edit.svg') }}" width="20" height="20" alt="Editar" title="Editar">
+                    </a>
+                    <button type="submit" class="btn btn-danger" onclick="if (!confirm('¿Esta seguro de borrar el profesor?')) return false;">
+                        <img src="{{ asset('svg/delete.svg') }}" width="20" height="20" alt="Borrar" title="Borrar">
+                    </button>
+                    {!!Form::close()!!}
+                </td>
+            </tr>
+     @if($loop->last)
+        </table>
+
+    @endif
+    @empty
+@endforelse
+    </div>
+
+@endsection
