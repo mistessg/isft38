ebfc44efdc1b0b7704abaafb219b8b0df8fd6c0a
BrianG02 <112129982+BrianG02@users.noreply.github.com>
2022-09-22 21:37:05 -0300

    programa pendientes bg

--- resources/views/frontend/programa/cargar_programa.blade.php
+++ resources/views/frontend/programa/cargar_programa.blade.php
@@ -8,9 +8,8 @@
   display:flex;
   justify-content:space-between;
   border-radius:70px;
-  margin:50px;
+  margin:65px;
 }
-
 </style>
 
 <div class="btn-group algo" role="group" aria-label="Basic example">
@@ -21,7 +20,65 @@
   <button style="border-radius:60px;" type="button" class="btn btn-success m-2">Programas Pendientes</button>
 </div>
 
+<div class="container my-4">
 
-<!--  -->
+  <div class="card">
+    <h5 class="card-header" style=" background-color: #181818; color: white;">Consulte su programa</h5>
+    <div class="card-body">
+
+      {{ Form::open(['route' => '']) }}
+      @csrf
+
+      <div class="input-group mb-3">
+        {{ Form::label("sede_id", 'Sede', ['class' => 'input-group-text']) }}
+        {{Form::select("sede_id", $sedes, $sede, ["class" => "form-select", "placeholder" => "Seleccione una sede"]) }}
+        @error('sede')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+      </div>
+
+
+      <div class="input-group mb-3">
+        {{ Form::label("carrera_id", 'Carrera', ['class' => 'input-group-text']) }}
+        {{Form::select("carrera_id", $carreras, $carrera, ["class" => "form-select", "placeholder" => "Seleccione una carrera"]) }}
+        @error('carrera')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+      </div>
+
+      <div class="input-group mb-3">
+        {{ Form::label("anio_id", 'Año', ['class' => 'input-group-text']) }}
+        {{Form::select("anio_id", $anios, $anio, ["class" => "form-select", "placeholder" => "Seleccione un año"]) }}
+        @error('carrera')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+      </div>
+
+      <div class="input-group mb-3">
+        {{ Form::label("materia_id", 'Materia', ['class' => 'input-group-text']) }}
+        {{Form::select("materia_id", $materias, $materia, ["class" => "form-select", "placeholder" => "Seleccione una materia"]) }}
+        @error('carrera')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+      </div>
+
+      <div class="input-group mb-3">
+        {{ Form::label("comision_id", 'Comisión', ['class' => 'input-group-text']) }}
+        {{Form::select("comision_id", $comisiones, $comision, ["class" => "form-select", "placeholder" => "Seleccione una comisión"]) }}
+        @error('carrera')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+      </div>
+
+
+
+
+      <div class="d-grid gap-2">
+        <button class="btn btn-outline-dark" type="submit" aria-label="consultar">Consultar</button>
+      </div>
+</div>
+
 
 @endsection
