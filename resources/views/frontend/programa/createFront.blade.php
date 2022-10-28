244f1cad6deee3c133f900723cf107e00718b924
aladner <97419404+aladner@users.noreply.github.com>
2022-10-27 21:31:11 -0300

    esto

--- resources/views/frontend/programa/createFront.blade.php
+++ resources/views/frontend/programa/createFront.blade.php
@@ -47,6 +47,32 @@
     <h1 class="TextoInicio">Nuevo Programa</h1>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        
+      </div>
+      <div class="modal-body">
+      <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+</div>
+      </div>
+      <div class="modal-footer">
+        
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div>
     @if(Session::has('status'))
     <div class="alert alert-success">{{ Session('status')}}</div>
@@ -83,7 +109,7 @@
 
     <div class="form-group">
         {{ Form::label("materia_id", __('MATERIAS'), ['class' => 'control-label']) }}
-        {{Form::select("materia_id", $materias, null, ["class" => "form-control", "placeholder" => "Seleccione una materia"]) }}
+        {{Form::select("materia_id", $materias, old("materia_id"), ["class" => "form-control", "placeholder" => "Seleccione una materia"]) }}
         @error('materia_id')
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
@@ -102,13 +128,7 @@
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
     </div>
-    <div class="form-group">
-        {{ Form::label("fechaentrega", __('FECHA DE ENTREGA'), ['class' => 'control-label']) }}
-        {{Form::date("fechaentrega", null, ["class" => "form-control"]) }}
-        @error('fechaentrega')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
+    
     <div class="form-group @if($errors->has('imagen')) has-error has-feedback @endif" style="text-align:center;">
         {{ Form::label("imagen", __('PROGRAMA'), ['class' => 'control-label']) }}
         <br>
@@ -128,6 +148,10 @@
     <button type="submit" id="boton" disabled class="btn btn-success btn-block container-fluid p-3">{{__('Guardar')}}</button>
 </div>
 {!!Form::close()!!}
+
+
+
+  
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <!-- Script Get Materias -->
@@ -179,7 +203,7 @@
             }
         });
     }
-
+   
     function searchCarreras(){
     var sede_id = document.getElementById('sede_id').value;
             //var sede_id = document.getElementById('sede_id').value;
@@ -219,7 +243,7 @@
   }
 
     $(document).ready(function() {
-
+        $("#exampleModal").modal("show");
         $('#anio_id').change(function() {
             search(); 
         });
