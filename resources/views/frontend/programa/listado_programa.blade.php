073822e5c61944c1498baebecaae7a8c3454bdcf
renovacionweb38 <112045817+renovacionweb38@users.noreply.github.com>
2022-10-28 19:17:15 -0300

    Fix accordion programas

--- resources/views/frontend/programa/listado_programa.blade.php
+++ resources/views/frontend/programa/listado_programa.blade.php
@@ -54,27 +54,27 @@
     }
   }
 
-  .links{
+  .links {
     width: 100%;
     height: 70%;
-    
+
   }
 
-  .accordion{
+  .accordion {
     width: 100%;
 
   }
 
-  .accordion-collapse{
+  .accordion-collapse {
     height: auto;
-  } 
+  }
 
-  .card{
+  .card {
     width: 80%;
     margin: auto;
   }
 
-  .accordion{
+  .accordion {
     width: 80%;
     margin: auto;
   }
@@ -84,109 +84,109 @@
   <h1 class="TextoInicio">Listado de programas</h1>
 </div>
 
- 
-  <div class="card">
-    <h5 class="card-header" style=" background-color: #181818; color: white;">Consulte su programa</h5>
-    <div class="card-body">
 
-      {{ Form::open(['route' => 'programas.search']) }}
-      @csrf
+<div class="card">
+  <h5 class="card-header" style=" background-color: #181818; color: white;">Consulte su programa</h5>
+  <div class="card-body">
 
-      <div class="input-group mb-3">
-        {{ Form::label("anio_id", 'Periodo', ['class' => 'input-group-text']) }}
-        {{Form::select("anio_id", $periodos, $periodo, ["class" => "form-select", "placeholder" => "Seleccione un período"]) }}
-        @error('año')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-      </div>
+    {{ Form::open(['route' => 'programas.search']) }}
+    @csrf
 
-      <div class="input-group mb-3">
-        {{ Form::label("sede_id", 'Sede', ['class' => 'input-group-text']) }}
-        {{Form::select("sede_id", $sedes, $sede, ["class" => "form-select", "placeholder" => "Seleccione una sede"]) }}
-        @error('sede')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-      </div>
-
-
-      <div class="input-group mb-3">
-        {{ Form::label("carrera_id", 'Carrera', ['class' => 'input-group-text']) }}
-        {{Form::select("carrera_id", $carreras, $carrera, ["class" => "form-select", "placeholder" => "Seleccione una carrera"]) }}
-        @error('carrera')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-
-      </div>
-
-      <div class="input-group mb-3">
-        {{ Form::label("comision_id", 'Comisión', ['class' => 'input-group-text']) }}
-        {{Form::select("comision_id", $comisiones, $comision, ["class" => "form-select", "placeholder" => "Seleccione una comisión"]) }}
-        @error('carrera')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-      </div>
-
-      <div class="d-grid gap-2">
-        <button class="btn btn-outline-dark" type="submit" aria-label="consultar">Consultar</button>
-      </div>
-
-
-      <br>
+    <div class="input-group mb-3">
+      {{ Form::label("anio_id", 'Periodo', ['class' => 'input-group-text']) }}
+      {{Form::select("anio_id", $periodos, $periodo, ["class" => "form-select", "placeholder" => "Seleccione un período"]) }}
+      @error('año')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
     </div>
+
+    <div class="input-group mb-3">
+      {{ Form::label("sede_id", 'Sede', ['class' => 'input-group-text']) }}
+      {{Form::select("sede_id", $sedes, $sede, ["class" => "form-select", "placeholder" => "Seleccione una sede"]) }}
+      @error('sede')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
+    </div>
+
+
+    <div class="input-group mb-3">
+      {{ Form::label("carrera_id", 'Carrera', ['class' => 'input-group-text']) }}
+      {{Form::select("carrera_id", $carreras, $carrera, ["class" => "form-select", "placeholder" => "Seleccione una carrera"]) }}
+      @error('carrera')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
+
+    </div>
+
+    <div class="input-group mb-3">
+      {{ Form::label("comision_id", 'Comisión', ['class' => 'input-group-text']) }}
+      {{Form::select("comision_id", $comisiones, $comision, ["class" => "form-select", "placeholder" => "Seleccione una comisión"]) }}
+      @error('carrera')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
+    </div>
+
+    <div class="d-grid gap-2">
+      <button class="btn btn-outline-dark" type="submit" aria-label="consultar">Consultar</button>
+    </div>
+
+
+    <br>
+  </div>
+</div>
+
+<br>
+
+
+<!-- ACORDEON -->
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    @foreach($anios as $a)
+    @php($titulo = $a->descripcion)
+
+    @foreach($programas as $programa)
+    @if($a->id == $programa->anio_id)
+
+    @if($titulo)
+    <h2 class="accordion-header" id="heading{{$a->id}}">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$a->id}}" aria-expanded="true" aria-controls="collapse{{$a->id}}">
+        {{$titulo}} <br>
+      </button>
+
+    </h2>
+    @endif
+    <div id="collapse{{$a->id}}" class="accordion-collapse collapse " aria-labelledby="heading{{$a->id}}" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        @if($titulo)
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">MATERIA</th>
+              <th scope="col">PROFESOR</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            @endif
+            <tr>
+
+              <td><a target="_blank" href="{{asset('./storage/'. $programa->nombrearchivo)}}" style="color:cornflowerblue">{{$programa->materia->descripcion}}</td>
+              <td>{{$programa->profesor->apellido}}-{{$programa->profesor->nombre}}</td>
+              @if($titulo)
+              @csrf
+          </tbody>
+          {!!Form::close()!!}
+          @php($titulo = '')
+          @endif
+          @endif
+          @endforeach
+        </table>
+
+      </div>
+    </div>
+    @endforeach
   </div>
 
-  <br>
-
-
-  <!-- ACORDEON -->
-  <div class="accordion" id="accordionExample">
-    <div class="accordion-item">
-      @foreach($anios as $a)
-      @php($titulo = $a->descripcion)
-
-      @foreach($programas as $programa)
-      @if($a->id == $programa->anio_id)
-
-      @if($titulo)
-      <h2 class="accordion-header" id="heading{{$a->id}}">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$a->id}}" aria-expanded="true" aria-controls="collapse{{$a->id}}">
-
-          {{$titulo}} <br>
-        </button>
-        @php($titulo = '')
-        @endif
-
-      </h2>
-      <div id="collapse{{$a->id}}" class="accordion-collapse collapse " aria-labelledby="heading{{$a->id}}}" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col" >MATERIA</th>
-                <th scope="col" >PROFESOR</th>
-                
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-
-                <td><a target="_blank" href="{{asset('./storage/'. $programa->nombrearchivo)}}"style="color:cornflowerblue">{{$programa->materia->descripcion}}</td>
-                <td>{{$programa->profesor->apellido}}-{{$programa->profesor->nombre}}</td>
-
-                @csrf
-            </tbody>
-            {!!Form::close()!!}
-            @endif
-            @endforeach
-          </table>
-          
-        </div>
-        </div>
-        
-      </div>
-      @endforeach
-    </div>
-  
 
   @endsection
 
