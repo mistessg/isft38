d29811492826045a48989f576b07af42f2b526f7
renovacionweb38 <112045817+renovacionweb38@users.noreply.github.com>
2022-11-09 23:33:35 -0300

    Carreras

--- resources/views/frontend/carreras/index.blade.php
+++ resources/views/frontend/carreras/index.blade.php
@@ -1,156 +1,171 @@
 @extends('frontend.layout.main')
 @section('content')
-
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <style>
+  .titulo {
+    text-align: center;
+    color: white;
+    border-top: 10px solid white;
+  }
 
-    .titulo{
-      text-align: center;
-      color: white;   
-      /* border-top: 3px solid white; */
-    }
-    .carreras{
-        display: flex;
-        flex-wrap: wrap;
-        margin: 20px;
-    }
-    .card-text{
+  .carreras {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 20px;
+  }
+
+  .card-text {
+    display: flex;
+    flex-direction: column;
+    text-overflow: ellipsis;
+    flex: 1;
+    margin: 10px;
+    padding: 5px;
+    text-align: center;
+  }
+
+  .card-text p {
+    text-align: justify;
+  }
+
+  @media (min-width: 1000px) {
+    .card-text {
       display: flex;
       flex-direction: column;
       text-overflow: ellipsis;
       flex: 1;
       margin: 10px;
-      padding:5px;
-      text-align: center;
-    }
-    .card-text p{
-      text-align: justify;
-    }
-    @media (min-width: 1000px){
-      .card-text{
-        display: flex;
-      flex-direction: column;
-      text-overflow: ellipsis;
-      flex: 1;
-      margin: 10px;
-      padding:5px;
+      padding: 5px;
       text-align: center;
       height: 17rem;
-      }
     }
-    .btn-group{
-      margin-top: 20px;
-    }
-    
-    
-    
+  }
+
+  .btn-group {
+    margin-top: 20px;
+  }
 
 
 
-.botonMateria {
- padding: 17px 40px;
- border-radius: 7px;
- border: 0;
- background-color: white;
- box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
- letter-spacing: 1.5px;
- text-transform: uppercase;
- font-size: 15px;
- transition: all .5s ease;
-}
 
-.botonMateria:hover {
- letter-spacing: 3px;
- background-color: #FF4A4A;
- color: hsl(0, 0%, 100%);
- box-shadow: #D61C4E 0px 7px 29px 0px;
-}
 
-.botonMateria:active {
- letter-spacing: 3px;
- background-color: hsl(261deg 80% 48%);
- color: hsl(0, 0%, 100%);
- box-shadow: rgb(93 24 220) 0px 0px 0px 0px;
- transform: translateY(10px);
- transition: 100ms;
-}
 
-.imagenCarrera{
-  width: 100%;
-  height: 300px;
-  object-fit: cover;
-}
-.modal-header{
-  background:#262626;
-  color:white;
-}
-.modal-footer{
-  background:#262626;
-}
-    
+  .botonMateria {
+    padding: 17px 40px;
+    border-radius: 7px;
+    border: 0;
+    background-color: white;
+    box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+    font-size: 15px;
+    transition: all .5s ease;
+  }
+
+  .botonMateria:hover {
+    letter-spacing: 3px;
+    background-color: #FF4A4A;
+    color: hsl(0, 0%, 100%);
+    box-shadow: #D61C4E 0px 7px 29px 0px;
+  }
+
+  .botonMateria:active {
+    letter-spacing: 3px;
+    background-color: hsl(261deg 80% 48%);
+    color: hsl(0, 0%, 100%);
+    box-shadow: rgb(93 24 220) 0px 0px 0px 0px;
+    transform: translateY(10px);
+    transition: 100ms;
+  }
+
+  .imagenCarrera {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+  }
+
+  .modal-header {
+    background: #262626;
+    color: white;
+  }
+
+  .modal-footer {
+    background: #262626;
+  }
 </style>
 
 <div class="titulo">
-    <h1>Carreras</h1>
+  <h1 data-aos="zoom-in-up" class="text-dark">Carreras</h1>
 </div>
 <div class="carreras">
 
   <div class="row">
-  @foreach ($carreras as $carrera)
+    @foreach ($carreras as $carrera)
     <div class="col-lg-6 col-md-12 mb-3 cards-texto">
-      <div class="card shadow-sm roberto">
-        <img src="{{asset('./storage/'. $carrera->imagen )}}" alt="" class="imagenCarrera">
-          <div class="card-body">
-          <div class="card-text">
-            <h4>{{ $carrera-> descripcion}}</h4>
-            <p>{{ $carrera-> texto }}</p></div>
-              <div class="d-flex justify-content-between align-items-center mt-4">
-                <div class="btn-group">
-                  <button id="{{$carrera->descripcion}}" onClick="reply_click(this.id)" type="button" class="botonMateria"data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$carrera->id}}">
-                    Materias
-                  </button>
-                </div>
-              </div>
-          </div>
-      </div>
-    </div>
-    @endforeach
-  </div>
-</div>
-
-<div>
-</div>
- 
-@foreach ($carreras as $carrera)
-<div class="modal fade" id="staticBackdrop{{$carrera->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel"><b> {{$carrera->descripcion}}</b></h1>
-        <button type="button" style="background-color:white;opacity:1;" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        @php($i = 0)
-        @foreach ($materias as $materia)
-          @if($materia->carrera_id == $carrera->id)
-           @if($i <> $materia->anio_id) 
-            @php($i =  $materia->anio_id)
-               <h5><b> {{$materia->deAnio->descripcion}}</b></h5>
-            @endif
-            <p style="word-wrap: break-word;font-size:16px;">- {{$materia->descripcion}}.</p>
+      @if($loop->index % 2 == 0)
+      <div class="card shadow-sm roberto" data-aos="flip-right">
+        @else
+        <div class=" card shadow-sm roberto" data-aos="flip-left">
           @endif
-        @endforeach
+          <img src="{{asset('./storage/'. $carrera->imagen )}}" alt="" class="imagenCarrera">
+          <div class="card-body">
+            <div class="card-text">
+              <h4>{{ $carrera-> descripcion}}</h4>
+              <p>{{ $carrera-> texto }}</p>
+              <p>Resolución: {{ $carrera->resolucion }}</p>
+            </div>
+            <div class="d-flex justify-content-between align-items-center mt-4">
+              <div class="btn-group">
+                <button id="{{$carrera->descripcion}}" onClick="reply_click(this.id)" type="button" class="botonMateria" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$carrera->id}}">
+                  Materias
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" style="font-weight:bold;background-color:white;color:#262626;opacity:1;" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      @endforeach
+    </div>
+  </div>
+
+  <div>
+  </div>
+
+  @foreach ($carreras as $carrera)
+  <div class="modal fade" id="staticBackdrop{{$carrera->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel"><b> {{$carrera->descripcion}}</b></h1>
+          <button type="button" style="background-color:white;opacity:1;" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          @php($i = 0)
+          @foreach ($materias as $materia)
+          @if($materia->carrera_id == $carrera->id)
+          @if($i <> $materia->anio_id)
+            @php($i = $materia->anio_id)
+            <h5><b> {{$materia->deAnio->descripcion}}</b></h5>
+            @endif
+            <p style="word-wrap: break-word;">- {{$materia->descripcion}}.</p>
+            @endif
+            @endforeach
+        </div>
+        <div class="modal-footer">
+          <button type="button" style="font-weight:bold;background-color:white;color:#262626;opacity:1;" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
-@endforeach
-<!-- <script type="text/javascript">
+  @endforeach
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+  <!-- <script type="text/javascript">
   function reply_click(clicked_id){
     alert(clicked_id);
   }
 </script> -->
 
-@endsection
+  @endsection
--- resources/views/frontend/layout/main.blade.php
+++ resources/views/frontend/layout/main.blade.php
@@ -174,14 +174,25 @@
   <!-- HEADER -->
   <nav class="navbar sticky-top navbar-dark bg-dark navbar-expand-lg m-auto ">
     <div class="container-fluid">
-      <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-        <img src="logo1.png" width="45px" alt="" />
+      <a href="{{ route('inicio') }}" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+        <img src="logo1.png" width="40px" alt="" />
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse rogelio" id="navbarToggleExternalContent">
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll nav-ul" style="--bs-scroll-height: 400px;">
+        <!--<ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll nav-ul" style="--bs-scroll-height: 400px;">-->
+        <ul class="navbar-nav d-flex justify-content-center w-100 navbar-nav-scroll nav-ul" style="--bs-scroll-height: 400px;">
+
+          <li class="nav-item">
+            <a href="{{ route('inicio') }}" class="nav-link border-effect">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+              </svg>
+              Inicio
+            </a>
+          </li>
           <li class="nav-item">
             <a href="{{ route('carreras') }}" class="nav-link border-effect">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
@@ -208,18 +219,17 @@
               </li>
               <li class="nav-li">
                 <a href="{{ route('programas') }}" class="nav-link d-flex justify-content-start">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
+                    <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z" />
+                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z" />
                   </svg>
                   Programas
                 </a>
               </li>
               <li class="nav-li">
                 <a href="http://docs.isft38.edu.ar/" class="nav-link d-flex justify-content-start">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-files" viewBox="0 0 16 16">
+                    <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
                   </svg>
                   DOCS
                 </a>
@@ -238,25 +248,36 @@
           </li>
           <li class="nav-item dropdown">
             <a href="{{ route('horarios.porCarrera') }}" class="nav-link" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-smartwatch" viewBox="0 0 16 16">
+                <path d="M9 5a.5.5 0 0 0-1 0v3H6a.5.5 0 0 0 0 1h2.5a.5.5 0 0 0 .5-.5V5z" />
+                <path d="M4 1.667v.383A2.5 2.5 0 0 0 2 4.5v7a2.5 2.5 0 0 0 2 2.45v.383C4 15.253 4.746 16 5.667 16h4.666c.92 0 1.667-.746 1.667-1.667v-.383a2.5 2.5 0 0 0 2-2.45V8h.5a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5H14v-.5a2.5 2.5 0 0 0-2-2.45v-.383C12 .747 11.254 0 10.333 0H5.667C4.747 0 4 .746 4 1.667zM4.5 3h7A1.5 1.5 0 0 1 13 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 11.5v-7A1.5 1.5 0 0 1 4.5 3z" />
               </svg>
               Horarios
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li class="nav-li">
                 <a href="{{ route('horarios.porCarrera') }}" class="nav-link d-flex justify-content-start">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                    <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+                  </svg>
                   Por Carrera
                 </a>
               </li>
               <li class="nav-li">
                 <a href="{{ route('horarios.porProfesor') }}" class="nav-link d-flex justify-content-start">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                  </svg>
                   Por Profesor
                 </a>
               </li>
               <li class="nav-li">
                 <a href="{{ route('horarios.PorDiaHora') }}" class="nav-link d-flex justify-content-start">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16">
+                    <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                  </svg>
                   Por dia / hora
                 </a>
               </li>
